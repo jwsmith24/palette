21d9ee7427d3dc2b647ff201336a0010728bc5f0
jake <jsmith181997@gmail.com>
2024-10-05 16:30:23 -0500

    add test react component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+import RubricSideBar from "./components/RubricSideBar.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RubricSideBar/>
   </StrictMode>,
 )
