bdba8fc596ac09230b57acf236151f36e0b9ca5a
jake <jsmith181997@gmail.com>
2024-10-05 16:56:47 -0500

    add icon and set up form component

---
+++ frontend/src/components/RubricForm.jsx
@@ -0,0 +1,13 @@
+export default function RubricForm() {
+    const students = [
+        {"asurite": "12345"}, {"asurite": "54321"}
+    ]
+
+    return (
+        <div>
+            <form>
+
+            </form>`
+        </div>
+    )
+}
