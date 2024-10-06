553a99bd95c0475cc4151ae23599922dcf15b538
Ben Aspinall <benjaminaspinall1@gmail.com>
2024-10-06 11:24:22 -0700

    learning how to create react components

---
+++ frontend/src/tests/practice_test.jsx
@@ -0,0 +1,11 @@
+import {describe, it} from "vitest";
+
+describe("Practice Suite", () => {
+    it("practice test", () => {
+        const sum = 3 + 3
+        expect(sum).toEqual(6) 
+
+    })
+
+
+})
