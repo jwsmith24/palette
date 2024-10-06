5e43f5997bf6aa04c5f93d39e043f58c60f48994
Ben Aspinall <benjaminaspinall1@gmail.com>
2024-10-06 11:41:53 -0700

    created a class based component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,15 @@
 import './App.css'
+import Component1 from './class_comp_example'
 
 function App() {
   
   return (
     <>
       <div className="App">
-      <h1>Palette</h1>
       <Component1/>
       </div>
       
     </>
   )
 }
-
-
-function Component1() {
-  
-  return (
-    <>
-      <div>
-      <h1>Example Component 1</h1>
-      <p>information...</p>
-      </div>
-      
-    </>
-  )
-}
-
 export default App
---
+++ frontend/src/class_comp_example.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+class Component1 extends React.Component{
+    render() {
+        return (
+            <>
+              <div>
+              <h1>Example Component 1</h1>
+              <p>information...</p>
+              </div>
+            </>
+          )
+    }
+}
+export default Component1
