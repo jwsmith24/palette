60441aa8be4fefdb6eec64edbd96fba28edc6703
jake <jsmith181997@gmail.com>
2024-10-07 12:25:00 -0400

    clean up code formatting

--- frontend/src/components/RubricSideBar.tsx
+++ frontend/src/components/RubricSideBar.tsx
@@ -1,8 +1,8 @@
 export default function RubricSideBar() {
-    return(
-        <div>
-            <h1>Group Progress</h1>
-            <p>Progress Bar</p>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Group Progress</h1>
+      <p>Progress Bar</p>
+    </div>
+  );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,jsx,ts,tsx}", // Include all source files
-    ],
-    theme: {
-        extend: {},
-    },
-    safelist: [
-        'bg-red-500',
-        'bg-yellow-500',
-        'bg-green-500',
-        'bg-blue-500',
-        'bg-purple-500',
-    ],
-    plugins: [],
-}
-
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all source files
+  ],
+  theme: {
+    extend: {},
+  },
+  safelist: [
+    "bg-red-500",
+    "bg-yellow-500",
+    "bg-green-500",
+    "bg-blue-500",
+    "bg-purple-500",
+  ],
+  plugins: [],
+};
