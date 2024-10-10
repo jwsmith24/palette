ddeaf9cd4b5d7185879927099129e351db8d75bb
jake <jsmith181997@gmail.com>
2024-10-10 09:21:56 -0500

    add comments to RatingProps interface

--- frontend/src/interfaces/RatingProps.ts
+++ frontend/src/interfaces/RatingProps.ts
@@ -3,5 +3,5 @@ Defines properties for Rating options that exist within each criterion.
  */
 export interface RatingProps {
   ratingCount: number;
-  criteriaIndex: number;
+  criteriaIndex: number; // used to track which criteria the rating belongs too to use as a key for React
 }
