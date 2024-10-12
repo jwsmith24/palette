4c5cb437a43e1cd07e26a506c23d4bafbead5455
jake <jsmith181997@gmail.com>
2024-10-12 16:22:40 -0500

    remove old bash script

--- start-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Navigating to frontend directory and starting the development server..."
-cd frontend || { echo "Failed to start the development server"; exit 1;}
-npm run dev &
-# let the server start up
-sleep 3
-# open the Vite dev server in default browser (works for WSL)
-cmd.exe /C start http://localhost:5173
-
