ecb27158a58335e77b87ab2989644447a7f682fc
jake <jsmith181997@gmail.com>
2024-10-12 13:17:53 -0500

    add command to automatically open web browser to vite server

--- start-dev.sh
+++ start-dev.sh
@@ -4,6 +4,6 @@ cd frontend || { echo "Failed to start the development server"; exit 1;}
 npm run dev &
 # let the server start up
 sleep 3
-# open the Vite dev server in default browser
+# open the Vite dev server in default browser (works for WSL)
 cmd.exe /C start http://localhost:5173
 
