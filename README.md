8a827a673715c29ad33e4839a86f4af3738a5b06
jake <jsmith181997@gmail.com>
2024-10-12 11:51:56 -0500

    add script to start and run dev server
    mac/linux and windows support

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Palette :art:
 
 An interactive rubric builder and grading assistant tool to improve the group project grading experience on Canvas.
+
+## Running the Application
+
+From the project directory run `start-dev.sh` on Mac/Linux or `start-dev.bat` on Windows.
+
---
+++ start-dev.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Navigating to frontend directory and starting the development server..."
+cd frontend || { echo "Failed to start the development server"; exit 1;}
+npm run dev &
+# let the server start up
+sleep 3
+# open the Vite dev server in default browser
+cmd.exe /C start http://localhost:5173
+
