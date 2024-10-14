fa9abc193d214545fa78fe7aecfbeba65468c314
jake <jsmith181997@gmail.com>
2024-10-14 15:53:31 -0500

    add basic 404 page to routing scheme

---
+++ frontend/src/components/util/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>404 - This is not the page you're looking for.</h1>
+      <p>Sorry, the page you're looking for doesn't exist.</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
