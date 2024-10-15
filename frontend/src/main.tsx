9ba6fb3b616a4a361f13cc85a63230045e59ec9c
Jake <155990103+jwsmith24@users.noreply.github.com>
2024-10-14 21:09:45 -0400

    US 51-Task 58 - Handle 404 Errors on Client-Side (#5)
    * add basic 404 page to routing scheme
    
    * style 404 page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,6 +8,7 @@ import "./index.css";
 import Home from "./components/home/Home.tsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RubricBuilder from "./components/rubric-builder/RubricBuilder.tsx";
+import NotFoundPage from "./components/util/NotFoundPage.tsx";
 
 // Defined a "root" div in index.html that we pull in here and then call the React render method.
 createRoot(document.getElementById("root")!).render(
@@ -18,6 +19,8 @@ createRoot(document.getElementById("root")!).render(
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/rubric-builder" element={<RubricBuilder />} />
+          {/*Any route that doesn't match the routes defined above will go to the 404 page*/}
+          <Route path={"*"} element={<NotFoundPage />} />
         </Routes>
       </div>
     </Router>
