adeb444a0ad1d8ac06126b3792ba8f993bcbce05
cmccand1 <clint.mccandless@gmail.com>
2024-10-17 11:12:50 -0700

    Improved some of the type declarations and added some documentation

--- frontend/src/Rubrics/Rubric.ts
+++ frontend/src/Rubrics/Rubric.ts
@@ -1,106 +1,79 @@
 // A Rubric object represents a grading rubric
 export interface Rubric {
-    id: number; // the ID of the rubric
-    title: string; // the title of the rubric
-    context_id: number; // the context owning the rubric
-    context_type: "Course" | string; // todo: what is context_type?
-    points_possible: number; // possible points for the rubric
+    id: number;                             // the ID of the rubric
+    title: string;                          // the title of the rubric
+    context_id: number;                     // the context owning the rubric
+    context_type: "Course" | string;        // todo: what is context_type?
+    points_possible: number;                // possible points for the rubric
     reusable: boolean;
     read_only: boolean;
     free_form_criterion_comments: boolean; // whether free-form comments are used
     hide_score_total: boolean;
-    data: RubricCriterion[] | null; // An array with all of this Rubric's grading Criteria
-    // If an assessment type is included in the 'include' parameter, includes an
-    // array of rubric assessment objects for a given rubric, based on the
-    // assessment type requested. If the user does not request an assessment type,
-    // this key will be absent.
+    data: RubricCriterion[] | null;        // An array with all of this Rubric's grading Criteria
+
+    /**
+     * (Optional) If an assessment type is included in the 'include' parameter, includes an
+     * array of rubric assessment objects for a given rubric, based on the
+     * assessment type requested. If the user does not request an assessment type,
+     * this key will be absent.
+     */
     assessments?: RubricAssessment[] | null;
-    // If an association type is included in the 'include' parameter, includes an
-    // array of rubric association objects for a given rubric, based on the
-    // association type requested. If the user does not request an association type,
-    // this key will be absent.
+
+    /**
+     * (Optional) If an association type is included in the 'include' parameter, includes an
+     * array of rubric association objects for a given rubric, based on the
+     * association type requested. If the user does not request an association type,
+     * this key will be absent.
+     */
     associations?: RubricAssociation[] | null;
 }
 
 // A RubricCriterion object represents a single criterion in a rubric
 export interface RubricCriterion {
-    // the ID of the criterion
-    id: string;
-    // short description of the criterion
-    description: string | null;
-    // long description of the criterion
-    long_description: string | null;
-    // maximum points for this criterion
-    points: number;
-    // whether the criterion uses a range of points
-    criterion_use_range: boolean;
-    // the possible ratings for this criterion
-    ratings: RubricRating[] | null;
+    id: string;                      // the ID of the criterion
+    description: string | null;      // short description of the criterion
+    long_description: string | null; // long description of the criterion
+    points: number;                  // maximum points for this criterion
+    criterion_use_range: boolean;    // whether the criterion uses a range of points
+    ratings: RubricRating[] | null;  // the possible ratings for this criterion
 }
 
 // A RubricRating object represents a rating for a specific criterion in a rubric
 export interface RubricRating {
-    // the ID of the rating
-    id: string;
-    // the ID of the associated criterion
-    criterion_id: string;
-    // short description of the rating
-    description: string | null;
-    // long description of the rating
-    long_description: string | null;
-    // points awarded for this rating
-    points: number;
+    id: string;                      // the ID of the rating
+    criterion_id: string;            // the ID of the associated criterion
+    description: string | null;      // short description of the rating
+    long_description: string | null; // long description of the rating
+    points: number;                  // points awarded for this rating
 }
 
 // A RubricAssessment object represents an assessment using a rubric
 export interface RubricAssessment {
-    // the ID of the rubric assessment
-    id: number;
-    // the rubric the assessment belongs to
-    rubric_id: number;
-    // the association ID for the rubric
-    rubric_association_id: number;
-    // the score given in this assessment
-    score: number;
-    // the type of object being assessed
-    // todo: verify this is the correct type
-    artifact_type: "Submission" | string;
-    // the ID of the object being assessed
-    artifact_id: number;
-    // number of attempts on the object being assessed
-    artifact_attempt: number;
-    // type of assessment, either 'grading', 'peer_review', or 'provisional_grade'
-    assessment_type: RubricAssessmentType;
-    // the ID of the user who made the assessment
-    assessor_id: number;
-    // (Optional) Full details of the assessment if 'full' is requested in the 'style' parameter
-    data?: any;
-    // (Optional) Comments only if 'comments_only' is requested in the 'style' parameter
-    comments?: any;
+    id: number;                            // the ID of the rubric assessment
+    rubric_id: number;                     // the rubric the assessment belongs to
+    rubric_association_id: number;         // the association ID for the rubric
+    score: number;                         // the score given in this assessment
+    artifact_type: "Submission" | string;  // the type of object being assessed
+    artifact_id: number;                   // the ID of the object being assessed
+    artifact_attempt: number;              // number of attempts on the object being assessed
+    assessment_type: RubricAssessmentType; // type of assessment, either 'grading', 'peer_review', or 'provisional_grade'
+    assessor_id: number;                   // the ID of the user who made the assessment
+    data?: any;                            // (Optional) Full details of the assessment if 'full' is requested in the 'style' parameter
+    comments?: any;                        // (Optional) Comments only if 'comments_only' is requested in the 'style' parameter
 }
 
 // A RubricAssociation object represents an association between a rubric and another entity
 export interface RubricAssociation {
-    // the ID of the association
-    id: number;
-    // the ID of the associated rubric
-    rubric_id: number;
-    // the ID of the object this association links to
-    association_id: number;
-    // the type of object this association links to
-    association_type: RubricAssociationType;
-    // whether the rubric is used for grading
-    use_for_grading: boolean;
-    // summary data for the association
-    summary_data: string;
-    // purpose of the association, either 'grading' or 'bookmark'
-    purpose: RubricAssociationPurpose
-    // whether the score total is displayed within the rubric
-    hide_score_total: boolean;
-    // whether points are hidden in the rubric
-    hide_points: boolean;
-    // whether outcome results are hidden
-    hide_outcome_results: boolean;
+    id: number;                              // the ID of the association
+    rubric_id: number;                       // the ID of the associated rubric
+    association_id: number;                  // the ID of the object this association links to
+    association_type: RubricAssociationType; // the type of object this association links to
+    use_for_grading: boolean;                // whether the rubric is used for grading
+    summary_data: string;                    // summary data for the association
+    purpose: RubricAssociationPurpose        // purpose of the association, either 'grading' or 'bookmark'
+    hide_score_total: boolean;               // whether the score total is displayed within the rubric
+    hide_points: boolean;                    // whether points are hidden in the rubric
+    hide_outcome_results: boolean;           // whether outcome results are hidden
 }
 
 // A RubricObjectHash object represents a response from the Rubric API for
