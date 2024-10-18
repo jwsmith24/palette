6257d4a658b11dc2843a7cd92445147462cb3ec3
jake <jsmith181997@gmail.com>
2024-10-18 14:39:04 -0500

    refactor rubric router to use express-async-handler
    removes repetive try/catch blocks and generalizes try/catch blocks for
    async functionality

--- backend/src/routes/rubricRouter.ts
+++ backend/src/routes/rubricRouter.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from "@prisma/client";
 import { body, validationResult } from "express-validator";
 import { Criteria } from "../../../frontend/src/models/types/criteria";
 import { Rating } from "../../../frontend/src/models/types/rating";
+import asyncHandler from "express-async-handler";
 
 const router = express.Router();
 const prisma = new PrismaClient();
@@ -31,16 +32,18 @@ const validateRubric = [
     .withMessage("points field must be numeric"),
 ];
 
-router.post("/rubrics", validateRubric, async (req: Request, res: Response) => {
-  console.log("got: ", req.body);
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(400).send({ errors: errors.array() });
-  }
+router.post(
+  "/rubrics",
+  validateRubric,
+  asyncHandler(async (req: Request, res: Response) => {
+    console.log("got: ", req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).send({ errors: errors.array() });
+    }
 
-  const { title, rubricCriteria } = req.body;
+    const { title, rubricCriteria } = req.body;
 
-  try {
     const newRubric = await prisma.rubric.create({
       data: {
         title,
@@ -60,16 +63,15 @@ router.post("/rubrics", validateRubric, async (req: Request, res: Response) => {
       },
     });
     res.status(201).send(newRubric);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ error: "Failed to create new rubric" });
-  }
-});
+  }),
+);
 
 // fetch all rubrics from the database
-router.get("/rubrics", async (req: Request, res: Response) => {
-  console.log("got", req.body);
-  try {
+router.get(
+  "/rubrics",
+  asyncHandler(async (req: Request, res: Response) => {
+    console.log("got", req.body);
+
     // gets all rubrics with their criteria and ratings
     const rubrics = await prisma.rubric.findMany({
       include: {
@@ -81,62 +83,52 @@ router.get("/rubrics", async (req: Request, res: Response) => {
       },
     });
     res.status(200).send(rubrics); // Send back list of all rubrics
-  } catch (error) {
-    res.status(500).send({ error: "Failed to get rubrics." });
-    console.log(error);
-  }
-});
+  }),
+);
 
 // update an existing rubric
 router.put(
   "/rubrics/:id",
   validateRubric,
-  async (req: Request, res: Response) => {
+  asyncHandler(async (req: Request, res: Response) => {
     const { id } = req.params;
     const { title, rubricCriteria } = req.body;
 
-    try {
-      const updatedRubric = await prisma.rubric.update({
-        where: { id: Number(id) },
-        data: {
-          title,
-          rubricCriteria: {
-            deleteMany: {}, // Deletes all existing criteria before adding new ones
-            create: rubricCriteria.map((criterion: Criteria) => ({
-              description: criterion.description,
-              longDescription: criterion.longDescription,
-              points: criterion.points,
-              ratings: {
-                create: criterion.ratings.map((rating: Rating) => ({
-                  description: rating.description,
-                  points: rating.points,
-                })),
-              },
-            })),
-          },
+    const updatedRubric = await prisma.rubric.update({
+      where: { id: Number(id) },
+      data: {
+        title,
+        rubricCriteria: {
+          deleteMany: {}, // Deletes all existing criteria before adding new ones
+          create: rubricCriteria.map((criterion: Criteria) => ({
+            description: criterion.description,
+            longDescription: criterion.longDescription,
+            points: criterion.points,
+            ratings: {
+              create: criterion.ratings.map((rating: Rating) => ({
+                description: rating.description,
+                points: rating.points,
+              })),
+            },
+          })),
         },
-      });
-      res.status(200).send(updatedRubric);
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({ error: "Failed to update rubric" });
-    }
-  },
+      },
+    });
+    res.status(200).send(updatedRubric);
+  }),
 );
 
 // delete an existing rubric
-router.delete("/rubrics/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
+router.delete(
+  "/rubrics/:id",
+  asyncHandler(async (req: Request, res: Response) => {
+    const { id } = req.params;
 
-  try {
     await prisma.rubric.delete({
       where: { id: Number(id) },
     });
     res.status(204).send(); // Deletion was successful
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ error: "Failed to delete rubric" });
-  }
-});
+  }),
+);
 
 export default router;
