1e79c269698e9215dd373d5b367581adfe08c7bd
jake <jsmith181997@gmail.com>
2024-10-18 20:04:48 -0500

    conditionally render csv import card

--- frontend/src/components/rubric-builder/CSVUpload.tsx
+++ frontend/src/components/rubric-builder/CSVUpload.tsx
@@ -70,13 +70,23 @@ const CsvUpload: React.FC<CsvUploadProps> = ({
       <h2 className="text-2xl font-bold text-gray-200 mb-4">
         Import CSV or XLSX
       </h2>
-      <input
-        type="file"
-        accept=".csv,.xlsx"
-        onChange={handleCsvUpload}
-        className="mt-4 mb-4 border border-gray-600 rounded-lg p-3 text-gray-300 hover:bg-gray-800 transition duration-300 cursor-pointer focus:outline-none"
-      />
-      {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
+      <div className={"flex justify-between items-center"}>
+        <input
+          type="file"
+          accept=".csv,.xlsx"
+          onChange={handleCsvUpload}
+          className="mt-4 mb-4 border border-gray-600 rounded-lg p-3 text-gray-300 hover:bg-gray-800 transition duration-300 cursor-pointer focus:outline-none"
+        />
+        {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
+
+        {/* Cancel Button */}
+        <button
+          onClick={closeImportCard}
+          className="h-10 mt-4 bg-red-600 text-white font-bold rounded-lg py-2 px-4 transition duration-300 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
+        >
+          Cancel
+        </button>
+      </div>
     </div>
   );
 };
