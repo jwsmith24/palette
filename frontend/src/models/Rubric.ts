35aaa3070a442119f389a98721696bb400860825
cmccand1 <clint.mccandless@gmail.com>
2024-10-22 13:53:50 -0700

    Refactor: Minor change to parameter name in createRubric method

--- frontend/src/models/Rubric.ts
+++ frontend/src/models/Rubric.ts
@@ -1,19 +1,19 @@
-import {RubricCriterion} from "./types/rubricCriterion.ts";
-import {Rubric} from "./types/rubric.ts";
-import {v4 as uuidv4} from "uuid";
+import { RubricCriterion } from "./types/rubricCriterion.ts";
+import { Rubric } from "./types/rubric.ts";
+import { v4 as uuidv4 } from "uuid";
 
 export default function createRubric(
-    title: string = "",
-    criteria: RubricCriterion[] = [],
-    description: string = "Enter description",
-    id: number | undefined = undefined, // defaults to undefined until the db assigns a unique id the database yet
-    // the database yet
+  title: string = "",
+  rubricCriteria: RubricCriterion[] = [],
+  description: string = "Enter description",
+  id: number | undefined = undefined, // defaults to undefined until the db assigns a unique id the database yet
+  // the database yet
 ): Rubric {
-    return {
-        title,
-        rubricCriteria: criteria,
-        description,
-        id,
-        key: uuidv4(),
-    };
+  return {
+    title,
+    rubricCriteria,
+    description,
+    id,
+    key: uuidv4(),
+  };
 }
