6d9039d0ce76d17f16648fb3ea77f9cbf2d1f7cb
jake <jsmith181997@gmail.com>
2024-10-26 10:41:12 -0500

    chore: format config files

--- backend/prisma/migrations/20241025142836_save_rubrics/migration.sql
+++ backend/prisma/migrations/20241025142836_save_rubrics/migration.sql
@@ -5,7 +5,9 @@ ALTER TABLE "RubricCriterion" DROP CONSTRAINT "RubricCriterion_rubricId_fkey";
 ALTER TABLE "RubricRating" DROP CONSTRAINT "RubricRating_criterionId_fkey";
 
 -- AddForeignKey
-ALTER TABLE "RubricCriterion" ADD CONSTRAINT "RubricCriterion_rubricId_fkey" FOREIGN KEY ("rubricId") REFERENCES "Rubric"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "RubricCriterion"
+    ADD CONSTRAINT "RubricCriterion_rubricId_fkey" FOREIGN KEY ("rubricId") REFERENCES "Rubric" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "RubricRating" ADD CONSTRAINT "RubricRating_criterionId_fkey" FOREIGN KEY ("criterionId") REFERENCES "RubricCriterion"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "RubricRating"
+    ADD CONSTRAINT "RubricRating_criterionId_fkey" FOREIGN KEY ("criterionId") REFERENCES "RubricCriterion" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
