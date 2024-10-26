b6c134a833c8158e84abc770e3286a3de55c9df8
jake <jsmith181997@gmail.com>
2024-10-25 21:16:31 -0500

    chore: update start.sh description
    - push to reset ci checks

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
-NC='\033[0m' # No color
+NC='\033[0m' # No color (to reset)
 
 # Check if Docker is installed
 if ! command -v docker &>/dev/null; then
