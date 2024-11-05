1986596457c09c4fc69b308296510fc0bae6683f
Ben Aspinall <benjaminaspinall1@gmail.com>
2024-11-05 09:10:01 -0700

    hotfix: undo changes from last commit

--- frontend/src/models/RubricCriterion.ts
+++ frontend/src/models/RubricCriterion.ts
@@ -5,7 +5,7 @@ export interface RubricCriterion {
   description: string;
   longDescription: string;
   ratings: RubricRating[];
-  id: number; // id is only assigned when the rubric is saved to the database
+  id?: number; // id is only assigned when the rubric is saved to the database
   points: number;
   template: string;
   updatePoints: () => void;
