8922e2750ef926d5eebe4266286898825ee2def7
Ben Aspinall <benjaminaspinall1@gmail.com>
2024-11-06 20:21:13 -0700

    bugfix: fix template.ts criteria import

--- frontend/src/models/Template.ts
+++ frontend/src/models/Template.ts
@@ -1,9 +1,9 @@
-import { RubricCriterion } from "./RubricCriterion.ts";
+import { Criteria } from "../../../palette-types/src/types/Criteria.ts";
 import { v4 as uuidv4 } from "uuid";
 
 export interface Template {
   title: string;
-  rubricCriteria: RubricCriterion[];
+  rubricCriteria: Criteria[];
   description: string;
   id?: number; // will be assigned by the backend once rubric is persisted
   key: string; // unique key for React DOM (with uuid)
@@ -11,9 +11,9 @@ export interface Template {
 
 export default function createTemplate(
   title: string = "",
-  criteria: RubricCriterion[] = [],
+  criteria: Criteria[] = [],
   description: string = "Enter description",
-  id: number = -1,
+  id: number = -1
 ): Template {
   return {
     title,
