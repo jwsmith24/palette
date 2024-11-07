f94c6babac23cab8309ec2acd46d700f7a075408
cmccand1 <clint.mccandless@gmail.com>
2024-11-07 01:47:39 -0700

    chore: add index.ts to multiple directories to simplify import statements across the backend

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Barrel file for controllers. Allows easy importing from a single
+ * 'controllers' alias without any messy config.
+ */
+
+// export everything from controllers
+export * from "./handleCreateRubricAssociation";
+export * from "./handleCreateRubric";
+export * from "./handleDeleteRubric";
+export * from "./handleGetAllRubrics";
+export * from "./handleGetRubricById";
+export * from "./handleGetRubricIdByTitle";
+export * from "./handleUpdateRubric";
---
+++ backend/src/middleware/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Barrel file for middleware. Allows easy importing from a single 'middleware' alias without any messy config.
+ */
+
+// export everything from middleware
+export * from "./rubricFieldErrorHandler";
+export * from "./rubricFieldErrorHandler";
+export * from "./requestLogger";
+export * from "./responseLogger";
+export * from "./fallbackErrorHandler"
