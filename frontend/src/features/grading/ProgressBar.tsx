6c1b88b4eae64dd0a42124c2f3e2929217b388e4
jake <jsmith181997@gmail.com>
2024-11-18 22:03:15 -0600

    fix: correct progress bars scrolling away

--- frontend/src/features/grading/ProgressBar.tsx
+++ frontend/src/features/grading/ProgressBar.tsx
@@ -15,17 +15,17 @@ export default function ProgressBar({ progress }: { progress: number }) {
   });
   return (
     <div className="bg-gray-800 p-2 rounded-2xl items-center grid">
-      <div className="relative w-full h-6 bg-gray-900 rounded-2xl overflow-hidden grid">
+      <div className="relative w-full h-6 bg-gray-900 rounded-2xl overflow-hidden">
+        {/* Progress Bar */}
         <div
-          className={`absolute top-0 left-0 h-full ${progressBarColor} hover:cursor-pointer`}
+          className={`absolute top-0 left-0 h-full ${progressBarColor}`}
           style={{ width: `${progress}%` }}
         ></div>
+        {/* Centered Progress Text */}
+        <div className="absolute inset-0 flex items-center justify-center">
+          <h2 className="text-white font-extrabold">{progress}% Graded</h2>
+        </div>
       </div>
-      <h2
-        className={`p-1 text-white absolute justify-self-center font-extrabold`}
-      >
-        {progress}% Graded
-      </h2>
     </div>
   );
 }
