f74becc4d29e79c8559562662796041f40ae848f
jake <jsmith181997@gmail.com>
2024-11-21 12:31:25 -0600

    fix: prettier

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,19 +2,19 @@
 Entry point for the entire application.
  */
 
-import {StrictMode} from "react";
-import {createRoot} from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import Home from "@features/home/Home.tsx";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RubricBuilderMain from "@features/rubricBuilder/RubricBuilderMain.tsx";
 import NotFoundPage from "./components/NotFoundPage";
 import UserRubrics from "@features/user/UserRubrics";
 import UserClusters from "@features/user/UserClusters";
 import GradingMain from "@features/grading/GradingMain.tsx";
-import {CourseProvider} from "./context/CourseProvider";
-import {AssignmentProvider} from "./context/AssignmentProvider.tsx";
+import { CourseProvider } from "./context/CourseProvider";
+import { AssignmentProvider } from "./context/AssignmentProvider.tsx";
 
 // Defined a "root" div in index.html that we pull in here and then call the React render method.
 createRoot(document.getElementById("root")!).render(
