ea47dfed7c2c2d1f432655481175d969f54bba18
jake <jsmith181997@gmail.com>
2024-11-26 19:52:25 -0600

    fix: update stray imports and improve push script
    - add build step to ensure the entire codebase compiles prior to pushing
      a new commit
    - #136

--- frontend/src/components/contextSelectors/NoCourseSelected.tsx
+++ frontend/src/components/contextSelectors/NoCourseSelected.tsx
@@ -3,8 +3,7 @@
  * @constructor
  */
 import { useState } from "react";
-import { Dialog } from "@components";
-import CourseSelectionMenu from "./CourseSelectionMenu.tsx";
+import { CourseSelectionMenu, Dialog } from "@components";
 
 export function NoCourseSelected() {
   const [courseMenuOpen, setCourseMenuOpen] = useState(false);
